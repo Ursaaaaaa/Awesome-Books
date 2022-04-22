9e68f448ebff3465dd9d412e7643eb1b2954b8c5
ramrod433 <djoletoamanor@gmail.com>
2022-04-22 13:22:28 +0000

    Update section buttons

--- books.js
+++ books.js
@@ -2,6 +2,30 @@ const bookList = document.querySelector('.books');
 const form = document.getElementById('added-book');
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
+const listMenu = document.querySelector('#list-menu');
+const formMenu = document.querySelector('#form-menu');
+const contactMenu = document.querySelector('#contact-menu');
+const formSec = document.querySelector('#add-new');
+const listSec = document.querySelector('#list');
+const contSec = document.querySelector('#contact');
+
+listMenu.addEventListener('click', () => {
+  formSec.style.display = 'none';
+  contSec.style.display = 'none';
+  listSec.style.display = 'block';
+});
+
+formMenu.addEventListener('click', () => {
+  formSec.style.display = 'block';
+  contSec.style.display = 'none';
+  listSec.style.display = 'none';
+});
+
+contactMenu.addEventListener('click', () => {
+  formSec.style.display = 'none';
+  contSec.style.display = 'block';
+  listSec.style.display = 'none';
+});
 
 let books = [];
 
